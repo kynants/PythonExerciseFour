7ad9689bbb87888d95ef0aff22ef331fb5143632
Kynan Stevenson <kynants@gmail.com>
2020-09-30 08:39:10 -0400

    Added user input of radius to the output.

--- program.py
+++ program.py
@@ -4,5 +4,6 @@ import math # used for π
 radius = int(input("Enter the circle's radius: "))
 area = math.pi * radius**2
 
-# output of A = πr^2
-print(area)
+# output
+print(radius)   # user input of radius
+print(area)     # calc of A = πr^2
